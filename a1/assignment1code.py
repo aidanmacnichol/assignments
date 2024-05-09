5457312cbd00683d64b2f39542c5eab5219301c6
Aidan MacNichol <108359181+aidanmacnichol@users.noreply.github.com>
2024-05-09 17:53:58 -0600

    Add files via upload

---
+++ a1/assignment1code.py
@@ -0,0 +1,11 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Aidan MacNichol
+
+studentName = "Aidan MacNichol"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+# print your best skill here
+print("Insanity is doing the same thing over and over again and expecting different results.\n-Einstien")
